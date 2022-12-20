3e96f2125e6c4d715382bddc2033d536e9b26ceb
Mikołaj Mejer <73598279+randomLando@users.noreply.github.com>
2022-12-20 09:19:52 +0100

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  if (a + b == c) {
+    cout << a << " + " << b << " == " << c << "\n";
+  }
+  if (a - b == c) {
+    cout << a << " - " << b << " == " << c << "\n";
+  }
+  if (a * b == c) {
+    cout << a << " * " << b << " == " << c << "\n";
+  }
+  if (a / b == c) {
+    cout << a << " / " << b << " == " << c << "\n";
+  }
+  if (a % b == c) {
+    cout << a << " % " << b << " == " << c << "\n";
+  }
+}
